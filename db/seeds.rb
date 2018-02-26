4491ae38de89d220c1692f0f9add8a95827e8a67
Nick Drishinski <ndrishinski@yahoo.com>
2018-02-26 16:26:24 -0700

    Sorta customized adminstrate to show enums

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 User created"
 
+AdminUser.create(email: "admin@admin.com", password: "adminadmin", password_confirmation: "adminadmin", first_name: "Admin", last_name: "Admin")
+
+puts "1 Admin User created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
