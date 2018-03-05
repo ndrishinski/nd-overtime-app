c9f6d303d7b9a47bc95719ef915c099671f894f4
Nick Drishinski <ndrishinski@yahoo.com>
2018-03-05 14:37:52 -0700

    added phone number to user and admin user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,16 @@ RSpec.describe User, type: :model do
 
     it "cannot be created without first_name, last_name" do
       @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "cannot be created without first_name, last_name" do
       @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "cannot be created without first_name, last_name" do
+      @user.phone = nil
       expect(@user).to_not be_valid
     end
   end
