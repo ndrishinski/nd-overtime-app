fc08e34f9a9ea2943bc1a8235776ff3469595f04
Nick Drishinski <ndrishinski@yahoo.com>
2018-03-07 12:05:27 -0700

    Created custom actions via button click for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy]
+  resources :audit_logs, except: [:new, :edit, :destory]
+  
   namespace :admin do
     resources :users
-    resources :admin_users
     resources :posts
+    resources :admin_users
 
     root to: "users#index"
   end
 
-  resources :posts
+  resources :posts do
+    member do
+      get :approve
+    end
+  end
+
   devise_for :users, skip: [:registrations]
- root to: 'static#homepage'
+  root to: 'static#homepage'
 end
